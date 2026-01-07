468af9cfede5f2f13f9c79049be456c8be005d11
Erki <mahuzon2@gmail.com>
2026-01-07 13:24:36 +0100

    [FEAT] Initial commit

---
+++ main/main.c
@@ -0,0 +1,34 @@
+//
+// Created by erki on 1/7/26.
+//
+
+#include <stdio.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+static const char *TAG="kconfig_demo";
+
+static const uint32_t sleep_time_ms = 1000;
+
+void delay(uint32_t timeout);
+
+void app_main(void)
+{
+    while (1)
+    {
+        printf("Log messages...\n");
+        ESP_LOGE(TAG, "Error");
+        ESP_LOGW(TAG, "Warn");
+        ESP_LOGI(TAG, "Info");
+        ESP_LOGD(TAG, "Debug");
+        ESP_LOGV(TAG, "Verbose");
+
+        delay(sleep_time_ms);
+    }
+}
+
+void delay(const uint32_t timeout)
+{
+    vTaskDelay(timeout/portTICK_PERIOD_MS);
+}
---
+++ scripts/export.sh
@@ -0,0 +1,1 @@
+. ~/esp/v5.3.2/export.sh
